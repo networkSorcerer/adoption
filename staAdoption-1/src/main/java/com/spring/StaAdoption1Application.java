058603d6baf13d10c3708cafd1ae58327e258620
networkSorcerer <gigattfyhig@gmail.com>
2024-03-10 00:20:31 +0900

    adoption

---
+++ staAdoption-1/src/main/java/com/spring/StaAdoption1Application.java
@@ -0,0 +1,13 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaAdoption1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaAdoption1Application.class, args);
+	}
+
+}
